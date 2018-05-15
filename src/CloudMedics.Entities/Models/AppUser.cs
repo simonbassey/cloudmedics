ea1fc3d440044f2b3a922ca8d81026a10e514df1
simonbassey <simonbassey@simonbasseys-MacBook-Pro.local>
2018-05-15 09:58:00 +0100

    Added a Entities project - Layer to contain domain models

---
+++ src/CloudMedics.Entities/Models/AppUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using CloudMedics.Entities.Enumerations;
+
+namespace CloudMedics.Entities
+{
+    public class AppUser {
+
+        [Key]
+        public Guid UserId { get; set; }
+        [Required]
+        public string FirstName  {get;set;}
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        public AccountType AccountType { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastUpdate { get; set; }
+        [Required]
+        public string CreatedBy { get; set; }
+    }
+}
