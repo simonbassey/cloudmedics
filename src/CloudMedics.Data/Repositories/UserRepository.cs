57e36f607e715b8234b8e9bdd9e28f0b0069fe1f
simonbassey <simon.dev.bassey@gmail.com>
2018-05-16 04:31:42 +0100

    Added User repository class to handle user data management

--- src/CloudMedics.Data/Repositories/UserRepository.cs
+++ src/CloudMedics.Data/Repositories/UserRepository.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CloudMedics.Entities;
+
 namespace CloudMedics.Data.Repositories
 {
-    public class UserRepository
+    public interface IUserRepository{
+        Task<AppUser> CreateUser(AppUser user);
+        Task<int> CreateUsers(IList<AppUser> users);
+        Task<IEnumerable<AppUser>> GetUsers();
+    }
+
+    public class UserRepository:BaseRepository<AppUser>, IUserRepository
     {
         public UserRepository()
         {
+        }
+
+        public async Task<AppUser> CreateUser(AppUser user)
+        {
+            return await base.Add(user);
+        }
+
+        public async Task<int> CreateUsers(IList<AppUser> users)
+        {
+            return await Add(users);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsers()
+        {
+            return await GetAll();
         }
     }
 }
