5a01f8b537834b133ab8945df117f083e43a5e33
simonbassey <simon.dev.bassey@gmail.com>
2018-05-16 04:30:27 +0100

    Added an abstraction for CRUD data management task

--- src/CloudMedics.Data/IRepository.cs
+++ src/CloudMedics.Data/IRepository.cs
@@ -1,7 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace CloudMedics.Data
 {
-    public interface IRepository
+    public interface IRepository<T> where T: class
     {
+        Task<T> Add(T entity);
+        Task<int> Add(IEnumerable<T> tEntities);
+        Task<T> Get(int entityId);
+        Task<IEnumerable<T>> GetAll();
+        Task<IEnumerable<T>> Filter(Func<T, bool> predicate);
     }
 }
