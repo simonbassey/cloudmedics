85b44d8ebe23496e43c57673a65ab8645997aa17
simonbassey <simon.dev.bassey@gmail.com>
2018-05-22 08:13:31 +0100

    Added a model for appointment

--- src/CloudMedics.Entities/Models/Appointment.cs
+++ src/CloudMedics.Entities/Models/Appointment.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using CloudMedics.Entities.Enumerations;
+
 namespace CloudMedics.Entities.Models
 {
     public class Appointment
     {
         public Appointment()
         {
+            Created = DateTime.Now;
         }
+
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public string AilmentDescription { get; set; }
+        public string Symptoms { get; set; }
+        public AppointmentStatus Status { get; set; }
+        public DateTime Created { get; set; }
+        [Required]
+        public DateTime ScheduledDate { get; set; }
+
+        public virtual Patient Patient { get; set; }
+        public virtual Doctor Doctor { get; set; }
     }
 }
