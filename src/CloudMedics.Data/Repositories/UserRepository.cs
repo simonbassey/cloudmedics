c741ba7e3e13c778c195d9bf1665b1e6ee6d2d1c
simonbassey <simon.dev.bassey@gmail.com>
2018-05-22 09:17:30 +0100

    updated user repository to add implementation for contracts

--- src/CloudMedics.Data/Repositories/UserRepository.cs
+++ src/CloudMedics.Data/Repositories/UserRepository.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using CloudMedics.Entities;
+using CloudMedics.Domain;
 
 namespace CloudMedics.Data.Repositories
 {
     public interface IUserRepository{
-        Task<AppUser> CreateUser(AppUser user);
-        Task<int> CreateUsers(IList<AppUser> users);
-        Task<IEnumerable<AppUser>> GetUsers();
+        
+        Task<AppUser> CreateUserAsync(AppUser user);
+        Task<int> CreateUsersAsync(IList<AppUser> users);
+        Task<AppUser> GetUserAsync(string userId);
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+        Task<AppUser> UpdateUserAccountAsync(string userId, AppUser updatedUser);
+        Task<bool> DeleteUserAccountAsync(string userId);
+        Task<IEnumerable<AppUser>> FilterUsersAsync(Func<AppUser, bool> filterFn);
+
     }
 
     public class UserRepository:BaseRepository<AppUser>, IUserRepository
@@ -17,19 +23,41 @@ namespace CloudMedics.Data.Repositories
         {
         }
 
-        public async Task<AppUser> CreateUser(AppUser user)
+        public async Task<bool> DeleteUserAccountAsync(string userId){
+            return await Delete(userId);
+        }
+
+        public async Task<AppUser> CreateUserAsync(AppUser user)
         {
             return await base.Add(user);
         }
 
-        public async Task<int> CreateUsers(IList<AppUser> users)
+        public async Task<int> CreateUsersAsync(IList<AppUser> users)
         {
             return await Add(users);
         }
 
-        public async Task<IEnumerable<AppUser>> GetUsers()
+        public async Task<IEnumerable<AppUser>> FilterUsersAsync(Func<AppUser, bool> filterFn)
+        {
+            return await Filter(filterFn);
+        }
+
+        public async Task<AppUser> GetUserAsync(string userId)
+        {
+            return await Get(userId);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
         {
             return await GetAll();
+        }
+
+        public async Task<AppUser> UpdateUserAccountAsync(string userId, AppUser updatedUser)
+        {
+            var userAccount = await GetUserAsync(userId);
+            if (userAccount == null)
+                return null;
+            return await Update(updatedUser);
         }
     }
 }
