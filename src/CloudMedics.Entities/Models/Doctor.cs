c5df5c9f545857b5a52b2b296057df59daa35fbb
simonbassey <simon.dev.bassey@gmail.com>
2018-05-22 08:09:05 +0100

    Addeded models for doctor and patient entities

--- src/CloudMedics.Entities/Models/Doctor.cs
+++ src/CloudMedics.Entities/Models/Doctor.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace CloudMedics.Entities.Models
 {
     public class Doctor
     {
-        public Doctor()
-        {
-        }
+        [Key]
+        public int DoctorId { get; set; }
+        public string UserId { get; set; }
+        public string ProfileSummary { get; set; }
+
+        public AppUser UserAccount { get; set; }
+        public ICollection<Appointment> Appointments {get;set;}
     }
 }
--- src/CloudMedics.Entities/Models/Patient.cs
+++ src/CloudMedics.Entities/Models/Patient.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CloudMedics.Entities.Enumerations;
+
 namespace CloudMedics.Entities.Models
 {
     public class Patient
     {
-        public Patient()
-        {
-        }
+        [Key]
+        public int PatientId { get; set; }
+        public int UserId { get; set; }
+        public string BloodGroup { get; set; }
+        public PatientType PatientType { get; set; }
+        public string Occupation { get; set; }
+
+        public virtual AppUser UserAccount { get; set; }
+        public ICollection<Appointment> Appointments { get; set; }
     }
 }
