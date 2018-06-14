a52c17908e7e9f86a5da22ce715346e9e7f5f371
simonbassey <simon.dev.bassey@gmail.com>
2018-06-14 22:57:42 +0100

    Added Helper method to test project

--- test/CloudMedics.API.Test/Helpers/TestHelper.cs
+++ test/CloudMedics.API.Test/Helpers/TestHelper.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
 namespace CloudMedics.API.Test.Helpers
 {
     public class TestHelper
     {
-        public TestHelper()
+        public static IConfigurationRoot GetApplicationConfiguration(string settingsBasePath = null)
         {
+
+            return new ConfigurationBuilder()
+                .SetBasePath(settingsBasePath ?? GetSettingsFilePath())
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
+        }
+
+        private static string GetSettingsFilePath()
+        {
+            string fullPath = typeof(TestHelper).GetTypeInfo().Assembly.Location;
+            return string.Empty;
         }
     }
 }
