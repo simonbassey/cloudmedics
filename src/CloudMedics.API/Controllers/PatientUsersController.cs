48a9faa696e90f0691d1f5c1b363915e43cbe7d2
simonbassey <simon.dev.bassey@gmail.com>
2018-06-17 21:30:30 +0100

    Created patient Account Controller

--- src/CloudMedics.API/Controllers/PatientUsersController.cs
+++ src/CloudMedics.API/Controllers/PatientUsersController.cs
@@ -1,10 +1,42 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using CouldMedics.Services;
+using System.Net;
+using System.Threading.Tasks;
+using CouldMedics.Services.Abstractions;
+using System.Linq;
+using CloudMedics.Domain.Models;
+
 namespace CloudMedics.API.Controllers
 {
-    public class PatientUsersController
+    [Route("api/users/patients")]
+    public class PatientUsersController : Controller
     {
-        public PatientUsersController()
+        private readonly IPatientUserService _patientUserService;
+        private readonly IUserService _accountService;
+        public PatientUsersController(IPatientUserService patientService, IUserService accountService)
         {
+            _patientUserService = patientService;
+            _accountService = accountService;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetPatients()
+        {
+            try
+            {
+                var patients = await _patientUserService.GetPatients();
+                foreach (Patient patient in patients)
+                {
+                    if (patient.UserAccount == null)
+                        patient.UserAccount = await _accountService.GetUserAsync(patient.UserId);
+                }
+                return Ok(patients);
+            }
+            catch (Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
         }
     }
 }
