41609ea55f2e8a6013f28a920bc3cf3468432cc7
simonbassey <simon.dev.bassey@gmail.com>
2018-06-17 21:28:36 +0100

    Created PatientUser service

--- src/CouldMedics.Services/PatientUserService.cs
+++ src/CouldMedics.Services/PatientUserService.cs
@@ -1,10 +1,42 @@
 ﻿using System;
+using CloudMedics.Domain.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using CloudMedics.Data;
+using CloudMedics.Data.Repositories;
+using System.Linq;
 namespace CouldMedics.Services
 {
-    public class PatientUserService
+    public interface IPatientUserService
     {
-        public PatientUserService()
+        Task<Patient> CreatePatient(Patient patient);
+        Task<List<Patient>> GetPatients();
+    }
+    public class PatientUserService : IPatientUserService
+    {
+        private readonly IPatientUserRepository _patientsRepository;
+        public PatientUserService(IPatientUserRepository patientUserRepository)
         {
+            _patientsRepository = patientUserRepository;
+        }
+
+
+        public Task<Patient> CreatePatient(Patient patient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Patient>> GetPatients()
+        {
+            try
+            {
+                var patientsList = await _patientsRepository.GetPatientsAsync();
+                return patientsList.ToList();
+            }
+            catch (Exception exception)
+            {
+                throw;
+            }
         }
     }
 }
