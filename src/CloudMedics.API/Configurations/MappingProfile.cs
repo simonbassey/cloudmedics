aea3edb2b1869a1f44adc9f6b2dde2802555d0cb
simonbassey <simon.dev.bassey@gmail.com>
2018-06-17 21:26:33 +0100

    Added Automapper configuarion

--- src/CloudMedics.API/Configurations/MappingProfile.cs
+++ src/CloudMedics.API/Configurations/MappingProfile.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using AutoMapper;
+using CloudMedics.Domain.ViewModels;
+using CloudMedics.Domain.Models;
+using CloudMedics.Domain.Enumerations;
+
 namespace CloudMedics.API.Configurations
 {
-    public class MappingProfile
+    public class MappingProfile : Profile
     {
         public MappingProfile()
         {
+            CreateMap<SignUpModel, ApplicationUser>()
+                .ForMember(u => u.Created, opt => opt.UseValue(DateTime.Now))
+                .ForMember(u => u.CreatedBy, opt => opt.UseValue("SYSTEM"))
+                .ForMember(u => u.LastUpdate, opt => opt.UseValue(DateTime.Now))
+                .ForMember(u => u.AccountStatus, opt => opt.UseValue(AccountStatus.Disabled))
+                .ForMember(u => u.UserName, opt => opt.MapFrom(s => s.Email))
+                .ForMember(u => u.AccountType, opt => opt.UseValue(AccountType.Patient));
+
         }
     }
 }
