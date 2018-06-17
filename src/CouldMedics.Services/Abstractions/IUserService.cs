d05e79acdf3ebc8da6cfe81319a648cd4fd7dc61
simonbassey <simon.dev.bassey@gmail.com>
2018-06-17 21:27:40 +0100

    Updated Account Service class

--- src/CouldMedics.Services/Abstractions/IUserService.cs
+++ src/CouldMedics.Services/Abstractions/IUserService.cs
@@ -6,9 +6,9 @@ using Microsoft.AspNetCore.Identity;
 
 namespace CouldMedics.Services.Abstractions
 {
-    public interface  IUserService
+    public interface IUserService
     {
-        Task<ApplicationUser> CreateUserAsync(ApplicationUser user);
+        Task<Tuple<IdentityResult, ApplicationUser>> CreateUserAsync(ApplicationUser user, string password = "");
         Task<ApplicationUser> GetUserAsync(string userId);
         Task<IEnumerable<ApplicationUser>> GetUsersAsync();
         Task<IEnumerable<ApplicationUser>> FilterUsersAsync(Func<ApplicationUser, bool> filterFn);
